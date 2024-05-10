c35d3f051140caa629a76810b213a28913eaac08
Sally P <sally@wiculty.com>
2024-05-10 21:29:39 +0530

    iterate 10 times

--- LoginUser.java
+++ LoginUser.java
@@ -1,7 +1,11 @@
 public class Login {
 	
-	public static void main()
+	public static void main(String args[])
 	{
 		int a;
+
+		for(i=0;i<=10;i++) {
+			System.out.println("Number: " + i);
+		}
 	}
 }
